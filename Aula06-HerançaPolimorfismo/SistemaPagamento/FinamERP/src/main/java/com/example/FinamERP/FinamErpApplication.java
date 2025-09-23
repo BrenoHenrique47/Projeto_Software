3d80e43cedf02b9657682b83bc24c37d7417195d
JorgeKayakiUMC <brenoohenrique47@gmail.com>
2025-09-23 10:30:07 -0300

    Aula08
    .

---
+++ Aula06-HerançaPolimorfismo/SistemaPagamento/FinamERP/src/main/java/com/example/FinamERP/FinamErpApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinamERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinamErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinamErpApplication.class, args);
+	}
+
+}
---
+++ Aula06-HerançaPolimorfismo/SistemaPagamento/FinamERP/src/test/java/com/example/FinamERP/FinamErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinamERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinamErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
